02f3de545e556cff21a4a9b43a71cee8992baf65
bandhan564 <jayasinghbandhan@gmail.com>
2022-05-08 14:32:06 +0530

    gift file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import {Gifts} from "./components/Gifts"
+function App() {
+
+
+  return (
+    <div className="App">
+       <Gifts/>
+    </div>
+  )
+}
+
+export default App
