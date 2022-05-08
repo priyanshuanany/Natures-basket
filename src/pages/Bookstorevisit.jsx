7a3e1dfd0f28519e53ae0b3acf22f35c0c70ac5c
manojGum <Manojgum@gmail.com>
2022-05-08 12:55:30 +0530

    not responsive

---
+++ src/pages/Bookstorevisit.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Bookstorevisit = () => {
+  return (
+    <div>Bookstorevisit</div>
+  )
+}
+
+export default Bookstorevisit
---
+++ src/pages/Healtyalternatives.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Healtyalternatives = () => {
+  return (
+    <div>Healtyalternatives</div>
+  )
+}
+
+export default Healtyalternatives
